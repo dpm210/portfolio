c5e528035f8dfefcde8af333cb6d0ee5b318e948
Daniel Mueller <danielpetermueller@gmail.com>
2021-09-01 14:45:42 -0400

    added links

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -4,12 +4,10 @@ function ProjectCard({project}){
 
     return (
         <div>
-                <Carousel>
                     <h2>{project.name}</h2>
                     <p>{project.description}</p>
                     <h4><a href={project.github_link}>Github</a></h4>
                     <h4><a href={project.demo_link}>Demo</a></h4>
-                </Carousel>
         </div>
     )
 }
